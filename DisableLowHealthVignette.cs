713bbc7a2933a81c9aa36f64a49771844200c3f7
Luiz <eldorado.luiz@gmail.com>
2023-04-16 15:08:59 -0300

    First commit

---
+++ DisableLowHealthVignette.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Modding;
+using UnityEngine;
+
+namespace DisableLowHealthVignette {
+    public class DisableLowHealthVignette : Mod, ITogglableMod {
+        new public string GetName() => "DisableLowHealthVignette";
+        public override string GetVersion() => "1.0.0.0";
+
+        public GameObject vignette;
+
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects) {
+            On.HUDCamera.OnEnable += OnHUDCameraOnEnable;
+            DisableVignette();
+        }
+
+        public void Unload() {
+            On.HUDCamera.OnEnable -= OnHUDCameraOnEnable;
+            EnableVignette();
+            vignette = null;
+        }
+
+        private void OnHUDCameraOnEnable(On.HUDCamera.orig_OnEnable orig, HUDCamera self) {
+            orig(self);
+
+            DisableVignette();
+        }
+
+        public void DisableVignette() {
+            vignette = GameObject.Find("Low Health Vignette");
+            if (vignette) {
+                vignette.SetActive(false);
+            }
+        }
+
+        public void EnableVignette() {
+            if (vignette) {
+                vignette.SetActive(true);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# DisableLowHealthVignette Hollow Knight Mod
+
+This mod disables the vignette (black stuff around the screen) that appears when you are at low health (1 hp or 1 mask).
